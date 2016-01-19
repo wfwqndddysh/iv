6309963f05fd49471b2e0533523229af17a21794
zhangjianjun <zhangfanwan@gmail.com>
2016-01-19 18:24:59 +0800

    c cnt

--- code.c
+++ code.c
@@ -844,3 +844,291 @@ printf("%lu\n", sizeof((x==2) ? f: 0)); //==sizeof(double)==sizeof(f)
 //sizeof在什么情况下求表达式的值?
 
 
+Array sizes are optional during array declaration by using external keyword.
+
+
+register static int a;  //，multiple storage classes in declaration specifier
+register auto int a;  //，multiple storage classes in declaration specifier
+register 变量不能取地址
+register 不能修饰全局变量
+
+void foo(auto int i) //error: storage class specified for parameter ‘i’
+
+
+#define foo(m, n) m ## n
+printf("%d\n", foo(2, 3)); //23
+printf("%s\n", foo(a, b)); //编译失败
+
+#define foo(m, n) "m ## n"
+printf("%s\n", foo(a, b)); //m ## n
+
+#define foo(m, n) #m #n
+printf("%s\n", foo(a, b)); //ab
+
+#define max 37;
+printf("%d", max);//编译失败
+
+#define A 1 + 2
+#define B 3 + 4
+int var = A * B; //var==11
+
+
+## macro operator
+<token> ## <token>
+The ## operator takes two separate tokens and pastes them together to form a 
+single token. The resulting token could be a variable name, class name or any 
+other identifier. A trivial example would be
+#define type i##nt
+type a; // same as int a; since i##nt pastes together to "int"
+Real world uses of the token pasting operator often involve class, variable, 
+or function names. For example, you might decide to create a macro that 
+declares two variables, one with a name based on parameter to the macro 
+and another with a different name, that stores the original value of the 
+variable (perhaps for debugging).
+#define DECLARE_AND_SET(type, varname, value) type varname = value; type orig_##varname = varname;
+Now you can write code like
+DECLARE_AND_SET( int, area, 2 * 6 );
+and orig_area always has the original value of area no matter how the variable is changed. 
+
+
+#<token>
+Prefixing a macro token with # will quote that macro token. This allows you to 
+turn bare words in your source code into text tokens. This can be particularly 
+useful for writing a macro to convert an enum into a string:
+enum ColorType
+{
+    CT_RED,
+    CT_GREEN,
+    CT_BLUE
+};
+
+note the use of CT to stand for the enum value
+// and #CT to create a string, such as "CT_RED"
+#define CONV_CASE(CT) case CT: return #CT
+std::string getString (ColorType t)
+{
+    switch ( t )
+    {
+        CONV_CASE( CT_RED );
+        CONV_CASE( CT_GREEN );
+        CONV_CASE( CT_BLUE );
+        default: return "unknown";
+    }
+}
+
+#define SYSTEM 20
+int main()
+{
+    int a = 20;
+#if SYSTEM == a
+    printf("HELLO ");
+#endif
+#if SYSTEM == 20
+    printf("WORLD\n");
+#endif
+}//WORLD
+
+
+#define MIN 0
+#if MIN
+#define MAX 10
+#endif
+printf("%d %d\n", MAX, MIN);//编译失败，MAX未定义
+
+
+The special operator defined is used in ‘#if’ and ‘#elif’ expressions to test 
+whether a certain name is defined as a macro. defined name and defined (name) 
+are both expressions whose value is 1 if name is defined as a macro at the 
+current point in the program, and 0 otherwise. Thus, #if defined MACRO is 
+precisely equivalent to #ifdef MACRO.
+
+defined is useful when you wish to test more than one macro for existence at once. For example,
+
+#if defined (__vax__) || defined (__ns16000__)
+would succeed if either of the names __vax__ or __ns16000__ is defined as a macro.
+
+Conditionals written like this:
+
+#if defined BUFSIZE && BUFSIZE >= 1024
+can generally be simplified to just #if BUFSIZE >= 1024, since if BUFSIZE is
+not defined, it will be interpreted as having the value zero.
+
+If the defined operator appears as a result of a macro expansion, the C
+standard says the behavior is undefined. GNU cpp treats it as a genuine defined
+operator and evaluates it normally. It will warn wherever your code uses this 
+feature if you use the command-line option -pedantic, since other compilers may
+handle it differently.
+
+
+int main(int argc, char **argv)
+{
+        printf("%d\n", argc);
+}
+./a.out > myfile argc==1, cat myfile: 1
+
+
+int i = 10, j = 2;
+printf("%d\n", printf("%d %d ", i, j));//10 2 5
+
+
+
+printf
+1.格式控制符使用说明
+
+printf的格式控制的完整格式:
+%[flags][width][.perc][F|N|h|l]type
+"%[- 0][m*][.n*][lh]type" type就是下文说的格式字符串
+下面对组成格式说明的各项加以说明:
+
+%:
+    %:  表示格式说明的起始符号,不可缺少。
+
+flags:
+    -:  有-表示左对齐输出(右侧补空格),如省略表示右对齐输出(左侧补空格)。
+    +:  在数字前增加符号+或-
+    0:  有0表示指定空位填0,如省略表示指定空位不填。
+     :  输出值为正时冠以空格，为负时冠以负号 
+    #:  
+
+width:
+    m:  m指域宽,即对应的输出项在输出设备上所占的字符数
+    *:  "*" or "*m$" (for some decimal integer m) to specify that the field 
+        width is given in the next argument, or in the m-th argument, 
+        respectively, which must be of type int.
+
+prec:
+    .n: n指精度。用于说明输出的实型 数的小数位数。对数值型的来说,未指定n时,隐含的精度为n=6位。
+    .*: "*"  or "*m$" (for some decimal integer m) to specify that the precision 
+        is given in the next argument, or in the m-th argument, respectively, 
+        which must be of type int.
+     .: If the precision is given as just '.', the precision is taken to be zero.
+        A negative  precision  is taken as if the precision were omitted.
+
+l/h:
+    l:  l对整型指long型,对实型指double型
+    h:  h用于将整型的格式字符修正为short型
+
+格式字符(格式字符用以指定输出项的数据类型和输出格式):
+    d格式:用来输出十进制整数,有几种用法: %d %hd %ld
+    i格式:用来输出十进制整数
+    o格式:以无符号八进制形式输出整数
+    x格式:以无符号十六进制形式输出整数, 小写
+    X格式:以无符号十六进制形式输出整数, 大写
+    f格式:用来输出实数(包括单精度,双精度),以小数形式输出
+    e,E  :用科学记数法表示浮点数
+    g,G  :用%f和%e表示中，总的位数最短的来表示浮点数
+    u格式:以无符号十进制形式输出整数
+    c格式:输出一个字符
+    p格式:输出一个指针
+    S格式:wchar_t字符（宽字符）类型字符串
+    s格式:用来输出一个串。有几中用法:
+        %s:例如:printf("%s","CHINA")输出"CHINA"字符串(不包括双引号)。
+        %ms:输出的字符串占m列,如字符串本身长度大于m,则突破获m的限制,将字符串全部输出。若串 长小于m,则左补空格。
+        %-ms:如果串长小于m,则在m列范围内,字符串向左靠,右补空格。
+        %m.ns:输出占m列,但只取字符串中左端n个字符。这n个字符输出在m列的右侧,左补空格,注 意:如果n未指定,默认为0。
+        %-m.ns:其中m、n含义同上,n个字符输出在m列范围的左侧,右补空格。如果n>m,则自动取n值,即保证n个字符正常输出,注意:如果n未指定,默认为0。
+
+        如果是sprintf(desc, "%m.ns", sour);
+        如果desc空间够的话,会在%m.ns 串 的结尾自动补null 字符,不同于strncpy。
+        例如 :sprintf(desc, "%.3s", "123456"); desc如果空间>=4字节的话,第4个字节将是null字 符。
+
+
+2.实型输出问题
+
+%f:不指定宽度,整数部分全部输出并输出6位小数。
+    // 默认输出6位小数
+    printf("%f", 3.1415926535f);
+    输出结果: 3.141593
+指定保留多少位小数
+    // 通过%.nf方式,指定保留多少位小数
+    printf("%.2f", 3.1415926535f);
+    输出结果: 3.14
+3.实型精度问题
+
+对于单精度数,使用%f格式符输出时,仅前7位是有效数字,小数6位.
+对于双精度数,使用%lf格式符输出时,前15位是有效数字,小数6位
+进制            float   double
+十进制有效位数  7位     15位
+二进制有效位数  24位    53位
+
+一个练习引发的血案
+要求输出3.1415926535f所有小数
+    // 尝试通过指定保留位数
+    printf("%.10f", 3.1415926535f);
+    输出结果: 3.1415927410
+    // float有效数字是7位, 多余位数则会显示垃圾数据(不准确)
+
+    // 什么是有效位?
+    printf("%.10f", 314159.26535f);
+    输出结果: 314159.2500000000
+    // 尝试指定宽度
+    printf("%12f", 3.1415926535f);
+    输出结果: $$$$3.141593 注意$代表空格
+    // 尝试指定宽度和保留位数
+    printf("%12.10f", 3.1415926535f);
+    输出结果: 3.1415927410
+    // 要想完整输出必须使用double, 因为double类型精度小数点后6位，有效数字是15位
+    double doubleValue = 3.1415926535; // 注意后面没有f
+    printf("%.10lf", doubleValue);
+    输出结果:3.1415926535
+
+
+
+scanf函数格式控制符
+
+格式            意义
+d               输入一个十进制整数
+o               输入一个八进制整数
+x               输入一个十六进制整数
+i               输入一个有符号或无符号的十进制、八进制、十六进制整数
+u               输入一个无符号十进制整数
+f,e或E,g或G     输入一个小数形式或指数形式的浮点数
+c               输入一个字符
+s               输入一个字符串
+获取整形数据
+    // 1.定义变量用于接收输入数据
+    int num = 0;
+    // 2.利用scanf函数接收用户输入数据
+    scanf("%d", &num);
+    // 3.打印验证结果
+    printf("num = %d", num);
+获取实数型数据
+全选复制放进笔记    // 1.定义变量用于接收输入数据
+    float num = 0;
+    // 2.利用scanf函数接收用户输入数据
+    scanf("%f", &num);
+    // 3.打印验证结果
+    printf("num = %d", num);
+注：不加\n,需要加&取地址
+非法的输入
+除"空格","回车","tab"之外, 如果输入的数据和scanf接收的数据不匹配将会自动结束接收
+
+
+
+const char *s = "myworld";
+const char *i = "myworld";
+int i = 9;
+printf("%*s", i, s); //两个空格myworld
+
+const char *s = "myworld";
+int i = 3;
+printf("%10.*s", i, s);//7个空格myw
+
+
+va_list start;
+va_start(start, n);//n是前一个参数
+int a = va_arg(start, int); 传给va_arg的类型必须是int类型而不能是char, so you 
+                            should pass ‘int’ not ‘char’ to ‘va_arg’
+                            传给va_arg的类型必须是double类型而不能是flost, so you 
+                            should pass ‘double’ not ‘flost’ to ‘va_arg’
+
+va_end(start)
+va_copy(start, dst);
+
+
+
+fprintf(stdout, "%d ", 45);
+fprintf(stderr, "%d ", 65);//65 45, 缓冲的原因
+
+
+
